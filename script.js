d7308087b94283e693e342332f01a1ccf4432a3d
Иззатилло Турсунов <izzatillotursunov@MacBook-Pro-Izzatillo.local>
2022-10-06 14:19:39 +0500

    api quotes

---
+++ script.js
@@ -0,0 +1,23 @@
+// Get Quotes from API
+let apiQuotes = [];
+// Show New Quote
+function newQuote(){
+ //Pick a random quote from apiQuotes array
+  let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes(){
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        alert('СУКААААА крашнулось все');
+        //Catch Error Here
+    }
+}
+
+//On load
+getQuotes();
